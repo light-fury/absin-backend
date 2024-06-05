43b2fe2619899fd5a47962b86b92aca59bcd0992
Light Fury <leech.developer@gmail.com>
2024-06-05 09:07:59 -0600

    fix: update port

--- src/index.ts
+++ src/index.ts
@@ -117,7 +117,7 @@ app.get('/points/:address/:eventName', async (req: Request, res: Response) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
